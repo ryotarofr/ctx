50e716375670215332c8da84fc7dba402b9809f9
ryotarofr <ryoryo.fr0608@gmail.com>
2025-05-23 17:40:01 +0900

    ini

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+// ■作るもの
+// python 用のデータ操作ライブラリ
+// とりあえずほしいの
+//
+// python関数を拡張するためのチェーンユーティリティを rust で書きたい
+// ■なぜつくるのか
+//  lambda 関数でデータ効率を担保しつつ、python コードの美しさをたもつ。
+//
+//
+// 可能なら db 操作からデータを直接ライブラリに取り込み内部関数のみでデータ操作振る舞うようにしたい
+// ネストされた構造をサポートするのは難しそう
+
+
+// def sum(a: int,b: int):
+//     return a + b
+//
+// sum(1, 2)
+//
+//
+// def get_data(params):
+//    sql = "SELECT * FROM my_table WHERE id = %s;"
+//    return db.SELECT(sql, params, is_all=True);
+//
+// res = get_data(tuple(100,)).is_none()
+
+pub struct CTX {
+
+}
+
